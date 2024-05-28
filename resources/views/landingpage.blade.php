f0fe76e74e3d9716b1304f3e832d173856806654
adepertiwi <adepertiwi05@gmail.com>
2024-05-28 10:39:57 +0800

    slicing fix

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -22,75 +22,70 @@
 
   {{-- hero section --}}
   <section class="bg-secondary flex items-center justify-center min-h-screen">
-    <div class="container mx-auto max-w-screen-xl p-4 ">
-      <div class="flex flex-col md:flex-row items-center justify-between">
-        <div class="mb-6 md:mb-0 md:mr-6 max-w-lg">
-          <h1 class="text-3xl md:text-4xl font-bold text-bold">Atasi Masalah Tidur dan Optimalkan Kualitas Tidur Anda</h1>
-          <p class="mt-4 text-gray-600">Rancang hidup tanpa stres di era kesadaran dan kesejahteraan. Bawa kedamaian ke malam Anda dengan metode yang terbukti—hanya dengan bernapas, rileks, dan tidur!</p>
-          <button href="#" class="text-white bg-primary hover:bg-gray-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 mt-6 text-center">Sign Up
-          </button>
+    <div class="container mx-auto max-w-screen-xl p-4">
+        <div class="flex flex-col md:flex-row items-center justify-between text-center md:text-left">
+            <div class="order-2 md:order-1 mb-6 md:mb-0 md:mr-6 max-w-lg">
+                <h1 class="text-3xl md:text-4xl font-bold text-bold">Atasi Masalah Tidur dan Optimalkan Kualitas Tidur Anda</h1>
+                <p class="mt-4 text-gray-600">Rancang hidup tanpa stres di era kesadaran dan kesejahteraan. Bawa kedamaian ke malam Anda dengan metode yang terbukti hanya dengan bernapas, rileks, dan tidur!</p>
+                <div class="flex justify-center md:justify-start mt-6 w-full md:w-auto order-3 md:order-none">
+                    <button href="#" class="text-white bg-primary hover:bg-gray-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">
+                        Mulai
+                    </button>
+                </div>
+            </div>
+            <img src="{{ asset('assets/hero-pic.svg') }}" alt="Sleeping Illustration" class="w-full object-contain  order-1 md:order-2">
         </div>
-        <img src={{asset('assets/hero-pic.svg')}} alt="Sleeping Illustration" class="w-full  object-contain">
-      </div>
     </div>
   </section>
 
+  {{-- artikel --}}
   <section class="items-center justify-center min-h-screen mt-40">
-    <h1 class="text-3xl md:text-4xl font-bold text-bold text-center mb-20">Artikel terkait Gangguan Tidur</h1>
     <div class="container mx-auto max-w-screen-xl p-4">
+      <h1 class="text-3xl md:text-4xl font-bold text-bold text-center mb-20">Artikel terkait Gangguan Tidur</h1>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {{-- card 1 --}}
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-              <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-          </a>
+        <div class="max-w-sm bg-secondary border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img class="w-full h-auto rounded-t-lg object-cover" src={{asset('assets/gambar-1.svg')}} alt="" />
           <div class="p-5">
-              <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-              <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  bg-primary rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none">
-                  Read more
-                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg>
-              </a>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-bold">
+              Badan Sakit saat Bangun Tidur?
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Pernahkah Anda mengalami badan sakit saat bangun tidur di pagi hari? Biasanya, hal ini terjadi jika Anda melakukan aktivitas berat...
+            </p>
+            <a href="#" class="text-blue-600 hover:underline hover:underline-offset-2 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+              Baca selengkapnya
+            </a>
           </div>
         </div>
         {{-- card 2 --}}
         <div class="max-w-sm bg-secondary border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-              <img class="rounded-t-lg w-full" src={{asset('assets/gambar-3.svg')}} alt="" />
-          </a>
+          <img class="w-full h-auto rounded-t-lg object-cover" src={{asset('assets/gambar-2.svg')}} alt="" />
           <div class="p-5">
-              <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-bold dark:text-white">Kenali Tanda Gangguan Tidur</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-              <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none">
-                  Read more
-                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg>
-              </a>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-bold">
+              Apa itu Microsleep?
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Pernahkah kamu tidak sadar sudah tertidur selama beberapa detik? Jika ya, kamu mungkin telah mengalami microsleep...
+            </p>
+            <a href="#" class="text-blue-600 hover:underline hover:underline-offset-2 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+              Baca selengkapnya
+            </a>
           </div>
         </div>
         {{-- card 3 --}}
         <div class="max-w-sm bg-secondary border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-              <img class="rounded-t-lg" src={{asset('assets/gambar-3.svg')}} alt="" />
-          </a>
+          <img class="w-full h-auto rounded-t-lg object-cover" src={{asset('assets/gambar-3.svg')}} alt="" />
           <div class="p-5">
-              <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kenali Tanda Gangguan Tidur</h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Pakar kesehatan tidur dr. Andreas Prasadja, RPSGT, menyebut ada 3 gejala dalam gangguan tidur. Apa bedanya ketiga gejala tersebut?</p>
-              <a href="#" class="text-sm font-bold text-bold hover:underline">
-                Baca selengkapnya
-                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg>
-              </a>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-bold">
+              Kenali Tanda Gangguan Tidur
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Pakar kesehatan tidur dr. Andreas Prasadja, RPSGT, menyebut ada 3 gejala dalam gangguan tidur. Apa bedanya ketiga gejala tersebut?
+            </p>
+            <a href="#" class="text-blue-600 hover:underline hover:underline-offset-2 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+              Baca selengkapnya
+            </a>
           </div>
         </div>
       </div>
@@ -105,7 +100,13 @@
     </div>
   </section>
 
-
-
+  <footer>
+    <div class="container mx-auto max-w-screen-xl p-4 mt-10">
+        <hr class="border-primary">
+        <div class="text-center mt-5 mb-10">
+          <h6 class="text-bold font-bold">&copy;2024SOMNO. All rights reserved.</h6>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
