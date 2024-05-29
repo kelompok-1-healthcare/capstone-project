8c923318757c69cd3616afcb1b06ee84beee4aa7
adepertiwi <adepertiwi05@gmail.com>
2024-05-29 23:56:03 +0800

    Radial chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,13 @@ module.exports = {
             colors: {
                 primary: "#025F80",
                 secondary: "#B9D6DB",
-                bold: "#034157"
+                bold: "#034157",
             },
         },
     },
-    plugins: [require("flowbite/plugin")],
+    plugins: [
+        require("flowbite/plugin")({
+            charts: true,
+        }),
+    ],
 };
